436a2a47cd46be2703520bce7ed3994dd82c8921
tatsushi.fukunaga <tatsushi1789@gmail.com>
2020-08-19 18:24:25 +0900

    Initial Commit

---
+++ FlappyBird/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlappyBird
+//
+//  Created by Tatsushi Fukunaga on 2020/08/19.
+//  Copyright © 2020 tatsushi.fukunaga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
